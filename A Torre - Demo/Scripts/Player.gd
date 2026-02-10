1e416a42ce8191d438fc94662d8a78e1c7a54b61
Hugo Rocha <132574651+hugorocha05@users.noreply.github.com>
2026-02-10 18:39:36 -0300

    Code update and cleaning for Player.gd

--- A Torre - Demo/Scripts/Player.gd
+++ A Torre - Demo/Scripts/Player.gd
@@ -1,31 +1,35 @@
 extends CharacterBody2D
 
-var player_max_speed := 300.0  # Speed of the player-character, can be changed
-var player_acceleration := 15.0
+# ----------------------------
+# Character variables
+# ----------------------------
+var max_speed := 300.0  # Max speed that the player-character can achieve
+var acceleration := 8.0  # Acceleration value of the player-character, permits smooth movement
+var input_vector := Vector2.ZERO  # The input vector is a Vector2 I'm using to determine the direction of movement with x and y values ranging from -1 to 1. Ex: (-1, 0) means the input vector is "moving left" and (0, 1) means it's "moving down"
 
-func update_velocity(delta):  # Basic player-character movement 
-	
-	var input_vector := Vector2.ZERO  # Resets input_vector
-	
+# ----------------------------
+# Movement
+# ----------------------------
+func get_input_vector() -> void: 
 	# Get input actions
-	input_vector.x = Input.get_axis("Move Left", "Move Right")
-	input_vector.y = Input.get_axis("Move Up", "Move Down")
+	input_vector = Input.get_vector("Move Left", "Move Right", "Move Up", "Move Down")  # get_vector() returns a result input vector from the 4 directional input strengths, the input actions follow this order: negative x, positive x, negative y, positive y. It is essentially the same as creating 4 if statements and manually changing the input vector value depending on the direction. This result vector is already normalized so we don't have to do it manually. We normalize the input vector to prevent uneven movement when going diagonally
 	
-	if input_vector != Vector2.ZERO:
-		input_vector = input_vector.normalized()  # We normalize the input vector to prevent uneven movement when going diagonally
-		apply_acceleration(input_vector, delta)
+func update_velocity(delta: float) -> void:
+	if input_vector != Vector2.ZERO:  # If there are movement inputs
+		apply_acceleration(delta)
 	else:
 		apply_friction(delta)
 		
-func apply_friction(delta):
-	velocity = velocity.lerp(Vector2.ZERO, Globals.FRICTION * delta)  # lerp gradually brings velocity from the current value to a null vector in increments according to the FRICTION value, creating a deceleration effect. We multiply by delta so the movement is frame independent
+func apply_acceleration(delta: float) -> void:
+	velocity = velocity.lerp(input_vector * max_speed, acceleration * delta)  # lerp gradually brings the velocity from the current value to the player-character's max speed in increments according to the player's acceleration, creating an acceleration effect. We multiply by delta so the movement is frame independent
 
-func apply_acceleration(input_vector: Vector2, delta):
-	velocity = velocity.lerp(input_vector * player_max_speed, player_acceleration * delta)  # lerp gradually brings the velocity from the current value to the player-character's max speed in increments according to the player's acceleration, creating an acceleration effect. We multiply by delta so the movement is frame independent
+func apply_friction(delta: float) -> void:
+	velocity = velocity.lerp(Vector2.ZERO, Globals.FRICTION * delta)  # lerp gradually brings velocity from the current value to a null vector in increments according to the FRICTION value, creating a deceleration effect. We multiply by delta so the movement is frame independent. Globals is the name of my singleton/autoload script that contains all the global/environmental variables (check Globals.gd)
 
-func _physics_process(delta):
-	update_velocity(delta)  # 8-Directional Movement
-	
-	print(velocity)
-
+# ----------------------------
+# Processing
+# ----------------------------
+func _physics_process(delta: float) -> void:
+	get_input_vector()
+	update_velocity(delta)
 	move_and_slide()  # Godot's built-in movement function, moves character based on the 'velocity' attribute
